2b628db3ccd93d9fdfc1c4825453d11fc74db666
Joeri de Gooijer <joerideg@gmail.com>
2015-04-29 16:33:38 +0200

    Fix alert template path

--- src/components/directives/alert/alert.directive.js
+++ src/components/directives/alert/alert.directive.js
@@ -8,7 +8,7 @@
   function alertDirective () {
     return {
       restrict: 'E',
-      templateUrl: 'components/directives/alert.directive.html',
+      templateUrl: 'components/directives/alert/alert.directive.html',
       scope: {
         message: '='
       }
