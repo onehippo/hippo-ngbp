5201e34a1d13774f889b90468cc4e842045df1d3
Joeri de Gooijer <joerideg@gmail.com>
2015-12-15 18:35:34 +0100

    Able to run unittests with PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -2,31 +2,49 @@ var hippoBuild = require('hippo-build');
 var customConfig = require('./build.conf.js');
 var cfg = hippoBuild.buildConfig(customConfig);
 
-module.exports = function(config) {
+module.exports = function (config) {
   var options = {};
 
   options.basePath = '.';
-  options.frameworks = ['jasmine'];
+  options.frameworks = ['systemjs', 'jasmine'];
   options.files = [
-    cfg.bowerDir + 'angular/angular.js',
-    cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
-    cfg.bowerDir + 'angular-mocks/angular-mocks.js',
-    cfg.npmDir + 'babel-core/external-helpers.js',
-    cfg.npmDir + 'systemjs/dist/system-polyfills.js',
-    cfg.npmDir + 'systemjs/dist/system-register-only.js',
-    cfg.dist.indexScript,
-    cfg.src.unitTests
+    'src/angularjs/*.spec.js',
+    'src/angularjs/**/*.spec.js'
   ];
+  options.systemjs = {
+    config: {
+      transpiler: 'babel',
+      defaultJSExtensions: true,
+      paths: {
+        'babel': cfg.npmDir + 'babel-core/browser.js',
+        'systemjs': cfg.npmDir + 'systemjs/dist/system.js',
+        'system-polyfills': cfg.npmDir + 'systemjs/dist/system-polyfills.js',
+        'es6-module-loader': cfg.npmDir + 'es6-module-loader/dist/es6-module-loader.js',
+        'phantomjs-polyfill': cfg.npmDir + 'phantomjs-polyfill/bind-polyfill.js'
+      }
+    },
+    includeFiles: [
+      cfg.bowerDir + 'angular/angular.js',
+      cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
+      cfg.bowerDir + 'angular-mocks/angular-mocks.js',
+      cfg.npmDir + 'babel-core/external-helpers.js',
+      cfg.npmDir + 'systemjs/dist/system-polyfills.js',
+      cfg.npmDir + 'systemjs/dist/system-register-only.js',
+      cfg.dist.indexScript
+    ]
+  };
   options.reporters = ['progress', 'coverage'];
   options.coverageReporter = {
-    reporters: [{
-      type: 'html'
-    }, {
-      type: 'text-summary'
-    }]
-  }
+    reporters: [
+      {
+        type: 'html'
+      }, {
+        type: 'text-summary'
+      }
+    ]
+  };
   options.autoWatch = false;
-  options.browsers = ['Chrome'];
+  options.browsers = ['PhantomJS'];
   options.singleRun = true;
   options.preprocessors = {};
   options.preprocessors[cfg.dist.indexScript] = ['sourcemap', 'coverage'];
