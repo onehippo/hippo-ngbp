141483526d81441f1602f8e49ce4a1831b9568b8
Joeri de Gooijer <joerideg@gmail.com>
2016-01-21 12:23:52 +0100

    Update to latest hippo-build

--- karma.conf.js
+++ karma.conf.js
@@ -5,13 +5,16 @@ var cfg = hippoBuild.buildConfig(customConfig);
 module.exports = function karmaConfig(config) {
   var options = cfg.karma;
 
-  options.files = [
+  options.systemjs.includeFiles = [
     cfg.bowerDir + 'angular/angular.js',
     cfg.bowerDir + 'angular-animate/angular-animate.js',
     cfg.bowerDir + 'angular-aria/angular-aria.js',
     cfg.bowerDir + 'angular-material/angular-material.js',
     cfg.bowerDir + 'angular-ui-router/release/angular-ui-router.js',
     cfg.bowerDir + 'angular-mocks/angular-mocks.js',
+  ];
+
+  options.files = [
     cfg.src.templates,
     cfg.src.scripts,
     cfg.src.unitTests,
