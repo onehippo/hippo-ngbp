967a317b281997f6ab4093664e12a75c67fe3847
Arthur Bogaart <a.bogaart@onehippo.com>
2016-06-15 02:40:27 +0200

    Update dependencies and enable eslint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,54 +6,64 @@ var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        vendor: ['angular', 'angular-animate', 'angular-aria', 'angular-ui-router', 'angular-material']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        chunkFilename: '[id].bundle.js',
-        publicPath: '/'
-    },
-    debug: true,
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: 'style/useable!css!postcss!'
-            },
-            {
-                test: /\.js$/,
-                loaders: ['ng-annotate', 'nginject?deprecate', 'babel?{"presets":["es2015"]}'],
-                exclude: /(node_modules)/
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png)\w*/,
-                loader: 'file'
-            },
-            {
-              test: /\.html$/,
-              loader: 'raw',
-              exclude: /(index.html)/
-            }
-        ]
-    },
-    postcss: function() {
-        return [
-            autoprefixer({browsers: ['last 5 versions']})
-        ];
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
-        new HtmlWebpackPlugin({
-            pushState: true,
-            filename: 'index.html',
-            inject: 'body',
-            template: 'src/index.html',
-            //favicon: 'img/favicon.ico',
-            hash: false
-        })
+  entry: {
+    index: './src/index.js',
+    vendor: ['angular', 'angular-animate', 'angular-aria', 'angular-ui-router', 'angular-material']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].bundle.js',
+    publicPath: '/'
+  },
+  debug: true,
+  devtool: 'source-map',
+  devServer: {
+    stats: 'minimal'
+  },
+  module: {
+    preLoaders: [
+      { 
+        test: /\.js?$/, 
+        loader: 'eslint', 
+        exclude: /node_modules/ 
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style/useable!css!postcss!'
+      },
+      {
+        test: /\.js$/,
+        loaders: ['ng-annotate', 'nginject?deprecate', 'babel?{"presets":["es2015"]}'],
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png)\w*/,
+        loader: 'file'
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw',
+        exclude: /(index.html)/
+      }
     ]
+  },
+  postcss: function () {
+    return [
+      autoprefixer({ browsers: ['last 5 versions'] })
+    ];
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
+    new HtmlWebpackPlugin({
+      pushState: true,
+      filename: 'index.html',
+      inject: 'body',
+      template: 'src/index.html',
+      //favicon: 'img/favicon.ico',
+      hash: false
+    })
+  ]
 };
