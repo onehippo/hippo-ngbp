903717e3aae774381b5375234076d78fe2ce77cc
Arthur Bogaart <arthur.bogaart@bloomreach.com>
2017-05-02 14:06:16 +0200

    Fix broken test

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,7 +3,7 @@
 
 import 'angular';
 import 'angular-mocks/angular-mocks';
-import './angularjs/hippo-ngbp';
+import './angularjs/hippo-ngbp.module';
 
 const testsContext = require.context('.', true, /.spec$/);
 testsContext.keys().forEach(testsContext);
