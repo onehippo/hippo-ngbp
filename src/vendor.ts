aa107d01e6616c00fddbffbd5d7b1ccde8d70359
Joeri de Gooijer <joerideg@gmail.com>
2017-08-30 11:20:05 +0200

    Depend on @bloomreach/frontend-build

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,11 +1,11 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
-
-import './vendor.scss';
+import 'hammerjs';
 
 import 'angular';
 import 'angular-animate';
 import 'angular-aria';
 import 'angular-material';
-import 'hammerjs';
+
+import './vendor.scss';
